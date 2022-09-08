c71ef2ac7bc2dfe8a8929497832b07753b34de9e
EmixamPP <dirksen.maxime@gmail.com>
2022-09-08 16:12:14 +0200

    fix med entry

--- server/google_form.php
+++ server/google_form.php
@@ -10,7 +10,7 @@ $nom = $_POST["nom"];
 $prenom = $_POST["prenom"];
 $matricule = $_POST["matricule"];
 $regio = $_POST["regio"];
-$med = $_POST["regio"];
+$med = $_POST["med"];
 $tel = $_POST["tel"];
 
 $conn = new mysqli(MYSQL_SERVER, MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_DB);

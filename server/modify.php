ff4c4d95437d76a9127c5139f29bbd13a7d2558b
ChristianAgb <agbendachristian@outlook.be>
2022-09-14 19:34:09 +0200

    modify.php fixed

--- server/modify.php
+++ server/modify.php
@@ -1,12 +1,9 @@
 <?php
 include_once("global.php");
 
-// Prepare json response
-header("Content-Type: application/json");
 
-if (!isset($_POST["matricule"]) || !isset($_POST["token"] || !isset($_POST["nom"]|| !isset($_POST["prenom"]|| !isset($_POST["key"]|| !isset($_POST["value"])) {
+if (!isset($_POST["matricule"]) || !isset($_POST["token"]) || !isset($_POST["nom"])|| !isset($_POST["prenom"])|| !isset($_POST["key"])|| !isset($_POST["value"])) {
     http_response_code(BAD_REQUEST);
-    echo "[]";  // return empty json array
     exit();
 }
 
@@ -19,7 +16,6 @@ $bleu_key = $_POST["value"];
 $conn = new mysqli(MYSQL_SERVER, MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_DB);
 if ($conn->connect_error) { // connection to the mysql db failed
     http_response_code(SERVER_ERROR);
-    echo "[]";
     exit();
 }
 
@@ -33,9 +29,10 @@ if (!checkAdmin($conn, $admin_matricule, $admin_token)) {
 
 // Execute query
 $query = "UPDATE LISTING FROM SET '$key'='$value' WHERE Nom='$bleu_last_name' AND Prenom='$bleu_first_name' ";
-$conn->query($query);
-
-
+$result = $conn->query($query);
 $conn->close();
+if(result)
 http_response_code(SUCCESS);
+
+http_response_code(BAD_REQUEST)
 exit();

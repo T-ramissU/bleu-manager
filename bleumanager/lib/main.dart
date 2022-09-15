2d92459f8ef1c6d570c576b72825ffd4e8731c99
EmixamPP <dirksen.maxime@gmail.com>
2022-09-15 10:54:07 +0200

    async Credential Load

--- bleumanager/lib/main.dart
+++ bleumanager/lib/main.dart
@@ -5,35 +5,44 @@ import 'package:bleumanager/widget/list_page.dart';
 import 'package:bleumanager/util/credential.dart';
 
 void main() async {
-  // required to load credential
-  WidgetsFlutterBinding.ensureInitialized();
-  // credential are loadead before running the app in order to determine
-  // which home page to display
-  bool credExists = await Credential().load();
-  runApp(App(credExists));
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
-  final bool credExists;
-  const App(this.credExists, {super.key});
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     final String title = "Bleu Manager ${DateTime.now().year}";
     final ListPage listPage = ListPage(title: title);
     final LoginPage loginPage = LoginPage(title: title);
-    final Widget home = credExists ? listPage : loginPage;
 
-    return MaterialApp(
-      title: 'Bleu Manager',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      routes: {
-        '/list': (context) => listPage,
-        '/login': (context) => loginPage,
-      },
-      home: home,
-    );
+    return FutureBuilder<bool>(
+        future: Credential().load(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final Widget home = snapshot.data! ? listPage : loginPage;
+            // Credential loaded
+            return MaterialApp(
+              title: 'Bleu Manager',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+              ),
+              routes: {
+                '/list': (context) => listPage,
+                '/login': (context) => loginPage,
+              },
+              home: home,
+            );
+          }
+          // Credential not yet loaded
+          else {
+            return const Center(
+              child: Image(
+                image: AssetImage('assets/polytech.png'),
+              ),
+            );
+          }
+        });
   }
 }

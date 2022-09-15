d670f9f5198294017f600ddb3ed4d0189cd2a6fa
EmixamPP <dirksen.maxime@gmail.com>
2022-09-15 09:51:17 +0200

    new form

--- server/google_form.php
+++ server/google_form.php
@@ -3,11 +3,10 @@ include_once("global.php");
 
 if (
     !isset($_POST["nom"]) || !isset($_POST["prenom"]) || !isset($_POST["regio"]) ||
-    !isset($_POST["med"]) || !isset($_POST["tel"]) ||
-    !isset($_POST["sexe"]) || !isset($_POST["adresse"]) || !isset($_POST["respLegal"]) ||
-    isset($_POST["telRespLegal"])
-) {
-    send_mail("Serveur bleusaille", "Attribut POST manquant lors de la requête par google form.");
+    !isset($_POST["tel"]) || !isset($_POST["sexe"]) || !isset($_POST["adresse"]) || 
+    !isset($_POST["resplegal"]) || !isset($_POST["numresplegal"]) || !isset($_POST["daten"]))
+{
+    send_mail("Serveur bleusaille", "Attribut POST manquant lors de la requete par google form.");
     http_response_code(BAD_REQUEST);
     exit();
 }
@@ -15,12 +14,12 @@ if (
 $nom = $_POST["nom"];
 $prenom = $_POST["prenom"];
 $regio = $_POST["regio"];
-$med = $_POST["med"];
 $tel = $_POST["tel"];
 $sexe = $_POST["sexe"];
 $adresse = $_POST["adresse"];
-$respLegal = $_POST["respLegal"];
-$telRespLegal = $_POST["telRespLegal"];
+$respLegal = $_POST["resplegal"];
+$telRespLegal = $_POST["numresplegal"];
+$dateN = $_POST["daten"];
 
 
 $conn = new mysqli(MYSQL_SERVER, MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_DB);
@@ -32,13 +31,14 @@ if ($conn->connect_error) { // connection to the mysql db failed
 }
 
 // Execute query
-$query = "INSERT INTO LISTING (Nom,Prenom,Sexe,DateN,Adresse,Med,Com,Tel,Regio,Supp,RespLegal,NumRespLegal) VALUES 
-('$nom','$prenom','$sexe','$dateN','$adresse','$med','$com','$tel','$regio','$supp','$respLegal','$telRespLegal')";
+$query = "INSERT INTO LISTING (Nom,Prenom,Sexe,DateN,Adresse,Med,Tel,Regio,Supp,RespLegal,NumRespLegal) VALUES 
+('$nom','$prenom','$sexe','$dateN','$adresse','$med','$tel','$regio','$supp','$respLegal','$telRespLegal')";
 $result = $conn->query($query);
 
 if (!$result) { // bad query
     send_mail("Serveur bleusaille", "Impossible d'executer la query sql lors de l'ajout d'un bleu par google form. 
-    Ses informations non ajoutee a la DB sont : '$nom' '$prenom' '$regio' '$med' '$tel' '$sexe' '$adresse' '$respLegal' '$telRespLegal'");
+    Ses informations non ajoutee a la DB sont : '$nom' '$prenom' '$regio' '$med' '$tel' '$sexe' '$adresse' '$respLegal' '$telRespLegal'.
+    Error sql : {$conn->error}.");
     $conn->close();
     http_response_code(BAD_REQUEST);
     exit();
